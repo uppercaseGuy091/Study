f480509faa0a4c13f328c2e2e77e8fbd8973dfe5
Konstantinos <61021816+uppercaseGuy091@users.noreply.github.com>
2020-03-02 14:49:12 +0100

    fixed the cards activity so it now counts right/wrong answers and goes to results activity when the questions are done. still need to fix the "skipped questions" so they are shown at the end. in the results activity, the onBackPressed() and the startOverBtn.onClickListener() crash, I don't know why though added app icon as well

--- app/src/main/java/com/lab1/study/ResultsActivity.java
+++ app/src/main/java/com/lab1/study/ResultsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
@@ -11,6 +13,7 @@ public class ResultsActivity extends AppCompatActivity {
     TextView resultsTv;
     TextView rightTv;
     TextView wrongTv;
+    Button startOverBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +28,36 @@ public class ResultsActivity extends AppCompatActivity {
         final int rightAns = getIntent.getIntExtra("correctAns", 0);
 
 
-
-
-
-        //THIS CLASS IS NOT COMPLETE, I JUST UPLOADED IT FOR ANYONE TO SEE/WORK ON
-
-
-
-
-
-
         resultsTv = findViewById(R.id.results);
         rightTv = findViewById(R.id.right);
         wrongTv = findViewById(R.id.wrong);
+        startOverBtn = findViewById(R.id.startAgain);
 
-        resultsTv.setText("Here are the results for user " + username + "for the subject of " + subject + "and deck " + deckId);
+        resultsTv.setText("Here are the results for user " + username + "\nfor the subject of " + subject + "\ndeck " + deckId);
         rightTv.setText("You knew " + rightAns + " of the questions, good job!");
         wrongTv.setText("You need to work on " + wrongAns + " of the questions.");
+
+
+        /*
+        //this crashes for some reason
+        startOverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResultsActivity.this, SubjectsActivity.class);
+                startActivity(intent);
+            }
+        });*/
+    }
+
+    //this is supposed to restart the previous activity entirely
+    //so it doesn't give indexOutOfBounds anc crash
+    //but it doesn't work as it should
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(ResultsActivity.this, CardsActivity.class);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
